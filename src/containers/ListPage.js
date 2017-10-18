36f8e4f89d7e12f6333fa4b1422955a0fe235383
byejude4RP <657401262@qq.com>
2017-10-18 08:03:40 +0800

    add player part

--- src/containers/ListPage.js
+++ src/containers/ListPage.js
@@ -1,4 +1,4 @@
-import react,{Component} from 'react';
+import React,{Component} from 'react';
 export default class extends Component{
     render(){
         const {currentIndex,songList} = this.props.location.state;
@@ -8,7 +8,7 @@ export default class extends Component{
                 sessionStorage.setItem('index',x);
                 }} className={currentIndex == i? 'active_li':''}>{item.songname}--{item.singername}</li>
         });
-         
+
         return(
            <div className="components-song-list">
                <h1><span>当前列表</span><em onClick={this.props.history.goBack}></em></h1>
